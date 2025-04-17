06dbd2eff131c10b0e2f68a3db5c68dfc9ce12f0
luisir <luisir@mskcc.org>
2025-04-17 14:56:26 -0400

    Adding migrations for queue and archive table for identity linking

--- src/Infrastructure/Data/Migrations/20250417180144_AddIdentityLinkingQueueAndArchiveTables.Designer.cs
+++ src/Infrastructure/Data/Migrations/20250417180144_AddIdentityLinkingQueueAndArchiveTables.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Mskcc.Tools.Idp.ConnectionsAggregator.Infrastructure.Data;
 
@@ -11,9 +12,11 @@ using Mskcc.Tools.Idp.ConnectionsAggregator.Infrastructure.Data;
 namespace Mskcc.Tools.Idp.ConnectionsAggregator.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250417180144_AddIdentityLinkingQueueAndArchiveTables")]
+    partial class AddIdentityLinkingQueueAndArchiveTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
